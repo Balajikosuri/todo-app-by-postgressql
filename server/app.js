c52b89d658a5c0b1a575f3a0c34824c8e77f5b43
Balaji Kosuri <123826865+Balajikosuri@users.noreply.github.com>
2024-01-10 14:24:21 +0530

    cors issue resolved

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
+const cors = require("cors");
 dotenv.config();
 
 const port = process.env.PORT;
 const todoRouter = require("./routes/todoRouter");
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/todos", todoRouter);
 
